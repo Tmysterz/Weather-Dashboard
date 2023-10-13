443f3b11983666cd40be18bded19c0bc302bcf30
unknown <terrencemccarthysnkrs@gmail.com>
2023-10-13 02:38:52 -0500

    added selected city info structure. not responsive yet

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -1,2 +1,11 @@
+// var currentCityDisplay = $("#selectedCity");
+// var currentDateDisplay = document.querySelectorAll("#currentDate");
 
+// function displayCurrentDate () {
+//     var currentDate = dayjs().format('M,D,YYYY');
+
+//     currentDateDisplay.text(currentDate);
+// }
+
+// displayCurrentDate();
 

390ad6a750683652318389cf4330e356ef381212
unknown <terrencemccarthysnkrs@gmail.com>
2023-10-18 19:55:27 -0500

    added containers for the future forecast

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -9,3 +9,29 @@
 
 // displayCurrentDate();
 
+// var weatherAPI = ('http://api.openweathermap.org/geo/1.0/direct?q={Dallas},{TX},{+1}&limit={limit}&appid={01a44a475a8c136ae41db9d0428f671a}')
+
+// fetch(weatherAPI)
+// .then (function(response) {
+//     return response.json()
+// }).then (function(data){
+//     console.log(data);
+// });
+
+
+var apiKey = '01a44a475a8c136ae41db9d0428f671a'
+
+
+function searchWeather () {
+    var cityInput = document.getElementById('cityInput');
+    var cityName = cityInput.value;
+    
+    if(!cityName) {
+        alert("Please enter a city name");
+        return;
+    }
+
+    var weatherApiURL = 'api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}'
+
+}
+
